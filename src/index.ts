6ea9bad60d398e593876c7725c7a00b419acc8f8
Kasador <hunterstevenshaw@gmail.com>
2024-12-08 11:15:01 -0800

    backend [update] >>> Shuffled the array of values, on load, assign the shuffed values into aria-valuetext values of each card, on click, change text content to aria-valuetext attribute

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,11 @@ import './index.scss'; // imports >>>
 /* REFS >>> 
     1) https://webpack.js.org/guides/typescript/
     2) https://stackoverflow.com/questions/45780272/how-to-define-an-array-of-strings-in-typescript-interface
-    3) https://www.typescriptlang.org/docs/handbook/2/everyday-types.html >>> THE HOLY BIBLE OF TYPESCRIPT
+    3) https://www.typescriptlang.org/docs/
+    4) https://www.typescriptlang.org/docs/handbook/ >>> THE HOLY BIBLE OF TYPESCRIPT <<<
+    5) https://www.typescriptlang.org/docs/handbook/2/everyday-types.html
+    6) https://stackoverflow.com/questions/29043135/javascript-one-line-if-else-else-if-statement
+    7) https://stackoverflow.com/questions/48083353/i-want-to-know-how-to-shuffle-an-array-in-typescript
 */
 
 console.log('JS (TypeScript) file connected.')
@@ -10,43 +14,51 @@ console.log('JS (TypeScript) file connected.')
 /* let test: string = "test";
 console.log(test); */
 
-const allCards: NodeListOf<Element> = document.querySelectorAll('.card'); // global variable for cards
+// global variable for cards >>> 
+const allCards: NodeListOf<Element> = document.querySelectorAll('.card');
 const oneCard = document.querySelector('.card');
 let pickTwo: number = 0; // state management 
 let pickedTwo: boolean = false; // state management 
+
 
 interface MatchOptions { // inferance objects are in CAPS >>> optional sets will end with "?" ex: type: "a" | type?: "b"
     type: "A" | "B" | "C"; // only options for the card types
 }
 
-const randomizeCard = (opts: MatchOptions[]) => {
-    console.log(cardTypes[0].type); // output "a"
-    console.log(cardTypes, "array of objects for card types");
+const shuffleCards = (opts: MatchOptions[]) => {
+    let currentIndex = opts.length; // increment 
 
-    // console.log(allCards)
-    allCards.forEach(card => {
-        card.ariaValueText = cardTypes[0].type; // append values types "A, B, C" to the cards
-        // card.textContent = card.ariaValueText;
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+        // Pick a remaining element...
+        let randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
 
-        // random the cards
-    })
+        // And swap it with the current element.
+        [opts[currentIndex], opts[randomIndex]] = [
+            opts[randomIndex], opts[currentIndex]
+        ];
+
+        // While there remain elements to shuffle.
+        while (currentIndex != 0) {
+            // Pick a remaining element.
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+
+            // And swap it with the current element.
+            [opts[currentIndex], opts[randomIndex]] = [
+                opts[randomIndex], opts[currentIndex]
+            ];
+        }
+        console.log(cardTypes, "Shuffled, Ready for Use")
+
+        return opts;
+    }
 }
 
 allCards.forEach(card => { // add event listener for cards
     card.addEventListener('click', () => {
         card.textContent = card.ariaValueText;
-        // if (!pickTwo && !pickedTwo) {
-        //     pickTwo = 1;
-        // } else if (pickTwo) {
-        //     pickTwo = 2;
-        // } else if (pickTwo === 2) {
-        //     pickTwo = 3;
-        //     pickedTwo = true;
-        // } else if (pickTwo === 3) {
-        //     pickedTwo = false;
-        //     pickTwo = 0;
-        // }
-
 
         if (pickTwo === 2) { // match or not, on another click, reset
             pickTwo = 1;
@@ -73,7 +85,7 @@ allCards.forEach(card => { // add event listener for cards
 })
 
 const compareMatch = (first: string, second: string) => {
-    
+    // compare the two picks
 } 
 
 const showCard = (id: string) => {
@@ -92,13 +104,18 @@ const cardTypes: MatchOptions[] = [ // array of objects within a stored variable
     // {type: "d" }, >>> this does NOT work, its not in the options
 ]
 
-randomizeCard(cardTypes); // functions expects 1, put array of objects inside variables to call function and use within
 
-// const allCards: NodeListOf<Element> = document.querySelectorAll('.card');
-// // console.log(allCards)
-// allCards.forEach(card => {
-//     card.addEventListener('click', () => {
-//         let getIdAttr: any = card.getAttribute('id');
-//         console.log(getIdAttr)
-//     });
-// })
+shuffleCards(cardTypes); // functions expects 1, put array of objects inside variables to call function and use within
+// console.log(cardTypes)
+
+const appendValuesGenerated = () => {
+    let i = 0;
+    allCards.forEach(card => {
+        console.log(card, `${cardTypes[i].type}`);
+        card.ariaValueText = cardTypes[i].type;
+        i++;
+    });
+}
+
+
+appendValuesGenerated()
